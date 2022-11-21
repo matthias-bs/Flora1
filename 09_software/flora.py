32b46d194a1ef24014172e17b52353e1c9ad6b82
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2022-11-21 23:21:16 +0100

    Update flora.py

--- 09_software/flora.py
+++ 09_software/flora.py
@@ -87,8 +87,8 @@ from ppretty import ppretty #for debugging only
 # Flora specific modules
 from settings import Settings, DEBUG, VERBOSITY, PROJECT_NAME, PROJECT_VERSION,\
                      PROJECT_BUILD, PROJECT_URL, GPIO_TANK_SENS_LOW, GPIO_TANK_SENS_EMPTY,\
-                     GPIO_PUMP_POWER, GPIO_PUMP_STATUS, PROCESSING_PERIOD, MESSAGE_TIMEOUT, \
-                     BATT_LOW, PUMP_BUSY_MAN,
+                     GPIO_PUMP_POWER, GPIO_PUMP_STATUS, PROCESSING_PERIOD, MESSAGE_TIMEOUT,\
+                     BATT_LOW, PUMP_BUSY_MAN
 from print_line import print_line, sd_notifier
 from gpio import GPIO
 from sensor import Sensor
